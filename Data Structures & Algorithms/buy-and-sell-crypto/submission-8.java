aab1c80e68002a89f43eefd3c5e85aa81cae90a4
Shiv Patel <42050563+Shivppatel@users.noreply.github.com>
2026-09-23 01:02:16 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minBuy = Integer.MAX_VALUE;
+        for(int price: prices){
+            int profit = price - minBuy;
+            maxProfit = Math.max(profit, maxProfit);
+            minBuy = Math.min(price, minBuy);
+        }
+        return maxProfit;
+    }
+}

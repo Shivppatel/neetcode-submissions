eb51743a6e798edc6a5fc8822e6a7a5054dd4874
Shiv Patel <42050563+Shivppatel@users.noreply.github.com>
2026-09-24 04:43:16 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,31 @@
+class MinStack {
+    ArrayDeque<Integer> stack;
+    ArrayDeque<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int res = stack.pop();
+        if (minStack.peek() == res){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}

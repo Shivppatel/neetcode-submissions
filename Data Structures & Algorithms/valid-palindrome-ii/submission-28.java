f29eb982c31a129ea077bb7d5d2c6ac19e6ddc5f
Shiv Patel <42050563+Shivppatel@users.noreply.github.com>
2026-09-23 23:27:32 -0400

    Add: valid-palindrome-ii - submission-28

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-28.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while(left <= right){
+            if (Character.isLetterOrDigit(s.charAt(left)) &&
+                Character.isLetterOrDigit(s.charAt(right))){
+                if (s.charAt(left) != s.charAt(right)){
+                    return isPalindrome(s.substring(left + 1, right + 1)) ||                        isPalindrome(s.substring(left, right));
+                }
+                left += 1;
+                right -= 1;
+            } else if (!Character.isLetterOrDigit(s.charAt(left))){
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        while(left <= right){
+            if (Character.isLetterOrDigit(s.charAt(left)) &&
+            Character.isLetterOrDigit(s.charAt(right))){
+                if (s.charAt(left) != s.charAt(right)){
+                    return false;
+                }
+                left += 1;
+                right -= 1;
+            } else if (!Character.isLetterOrDigit(s.charAt(left))){
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return true;
+    }
+}

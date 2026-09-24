6b53c3264283d6da6bcd4e42c3f352a60fad5bd1
Shiv Patel <42050563+Shivppatel@users.noreply.github.com>
2026-09-23 23:40:55 -0400

    Add: three-integer-sum - submission-25

---
+++ Data Structures & Algorithms/three-integer-sum/submission-25.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for(int idx = 0; idx < nums.length - 2; idx++){
+            int left = idx + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[idx] + nums[left] + nums[right];
+                if (sum == 0){
+                    set.add(List.of(nums[idx], nums[left], nums[right]));
+                    left += 1;
+                    right -= 1;
+                } else if (sum > 0){
+                    right -= 1;
+                } else {
+                    left += 1;
+                }
+            }
+        }
+        List<List<Integer>> output = new ArrayList<>();
+        for(List<Integer> list: set){
+            output.add(list);
+        }
+        return output;
+    }
+}
